50c2d3fb7b753902b83551d8b67326654e266341
jordan <jharrison1@qa.com>
2021-06-08 14:57:10 +0100

    added entities

--- src/main/java/com/qa/cars/domain/Car.java
+++ src/main/java/com/qa/cars/domain/Car.java
@@ -1,14 +1,31 @@
 package com.qa.cars.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity // marks a class as representing a table in the db
 public class Car {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT
+	private Integer id;
+
+	@Column(name = "bloop", unique = true)
 	private String make;
 
 	private String model;
 
 	private String colour;
 
+	@ManyToOne
+	private Garage garage;
+
 	public Car() {
+		// REQUIRED
 	}
 
 	public Car(String make, String model, String colour) {
@@ -16,6 +33,16 @@ public class Car {
 		this.make = make;
 		this.model = model;
 		this.colour = colour;
+	}
+
+	// REQUIRED
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
 	}
 
 	public String getMake() {
@@ -40,6 +67,14 @@ public class Car {
 
 	public void setColour(String colour) {
 		this.colour = colour;
+	}
+
+	public Garage getGarage() {
+		return garage;
+	}
+
+	public void setGarage(Garage garage) {
+		this.garage = garage;
 	}
 
 	@Override
---
+++ src/main/java/com/qa/cars/domain/Garage.java
@@ -0,0 +1,47 @@
+package com.qa.cars.domain;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Garage {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	private String name;
+
+	@OneToMany(mappedBy = "garage")
+	private List<Car> cars;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Car> getCars() {
+		return cars;
+	}
+
+	public void setCars(List<Car> cars) {
+		this.cars = cars;
+	}
+
+}
